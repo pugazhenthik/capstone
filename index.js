8c763d7041d9f730b3301376761a5ed01fdba3ed
pugazhenthik <pugazhwoo@gmail.com>
2023-06-23 21:35:59 +0530

    DB setup done

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config()
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("DB connected..")
+    })
+    .catch(error => {console.log('DB not connected '+ error)})
+
+    app.get('/', (req, res) => {
+    res.send('hello there')
+})
 
 app.listen(3000, () => {
     console.log('Server is running...')
